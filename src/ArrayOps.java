fba69b1c2f1dda7ce65b22e7f026e12947f122be
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-06-15 15:53:09 +0530

    create class

---
+++ src/ArrayOps.java
@@ -0,0 +1,19 @@
+/*
+ * insert at end
+ * insert at index
+ * delete from end
+ * delete from index
+ * grow
+ * search
+ * sort
+ * print
+ */
+public class ArrayOps {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
