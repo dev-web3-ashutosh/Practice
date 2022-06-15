fe2c602248a573a47d6e691fc145ad69a789d4ec
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2022-06-15 15:45:25 +0530

    write main method

--- src/StudlyCaps.java
+++ src/StudlyCaps.java
@@ -9,7 +9,36 @@ public class StudlyCaps {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Enter input string");
+		input = sc.nextLine();
 		
+		for(int i=0; i<input.length(); i++) {
+			ascii = input.charAt(i);
+			
+			if(i%2 == 0) {
+				if(ascii>=65 && ascii<=90) {
+					ascii += 32;
+					output += (char) ascii;
+				}
+				else {
+					output += (char) ascii;
+				}
+			} //end if
+			else {
+				if(ascii>=97 && ascii<=122) {
+					ascii -= 32;
+					output += (char) ascii;
+				}
+				else {
+					output += (char) ascii;
+				}
+			} //end else
+		} //end for
+		
+		System.out.println("Input string in studly caps is:");
+		System.out.println(output);
+		
+		sc.close();
 
 	}
 
