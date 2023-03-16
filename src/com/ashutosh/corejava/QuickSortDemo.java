19f8647310c33567646eca5dddf64b437bb42889
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-03-16 16:33:45 +0530

    define method partition

--- src/com/ashutosh/corejava/QuickSortDemo.java
+++ src/com/ashutosh/corejava/QuickSortDemo.java
@@ -1,6 +1,20 @@
 package com.ashutosh.corejava;
 
 public class QuickSortDemo {
+	int partition(int[] arr, int l, int h) {
+		int pivot=arr[h];
+		int j=l-1;
+		for(int i=0;i<h;++i) {
+			if(arr[i]<pivot) {
+				j++;
+				swap(arr,i,j);
+			}
+			swap(arr,j+1,h);
+			return (j+1);
+		}
+	}
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
