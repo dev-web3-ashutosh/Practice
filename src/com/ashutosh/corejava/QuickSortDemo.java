487a6cacfd095a7c99303e0f10b98f5b2271b135
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-03-16 16:37:17 +0530

    define method print2

--- src/com/ashutosh/corejava/QuickSortDemo.java
+++ src/com/ashutosh/corejava/QuickSortDemo.java
@@ -26,6 +26,13 @@ public class QuickSortDemo {
 		arr[j]=temp;
 	}
 	
+	void print2(int[] arr) {
+		int n=arr.length;
+		for(int i=0;i<n;++i) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
