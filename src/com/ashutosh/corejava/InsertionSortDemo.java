003f0c6b83f5402d6104d821cadb5b45e70c3f2b
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-03-16 16:01:10 +0530

    define method print

--- src/com/ashutosh/corejava/InsertionSortDemo.java
+++ src/com/ashutosh/corejava/InsertionSortDemo.java
@@ -13,6 +13,14 @@ public class InsertionSortDemo {
 			arr[j+1]=key;
 		}
 	}
+	
+	void print(int[] arr) {
+		int n=arr.length;
+		for(int i=0;i<n;++i) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
 
 	public static void main(String[] args) {
 		
