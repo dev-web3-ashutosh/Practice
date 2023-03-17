d3f45e613d5005573cced13b002868f54035b44b
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-03-17 12:53:17 +0530

    define method mergeSort

--- src/com/ashutosh/corejava/MergeSortDemo2.java
+++ src/com/ashutosh/corejava/MergeSortDemo2.java
@@ -36,6 +36,15 @@ public class MergeSortDemo2 {
 		}
 	}
 	
+	void mergeSort(int[] arr, int l, int r) {
+		if(l<r) {
+			int m=(l+r)/2;
+			mergeSort(arr,l,m);
+			mergeSort(arr,m+1,r);
+			merge(arr,l,m,r);
+		}
+	}
+	
 	
 
 	public static void main(String[] args) {
