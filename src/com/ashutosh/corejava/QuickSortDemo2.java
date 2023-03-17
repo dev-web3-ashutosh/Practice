9b6ad6a1b22a7a24a211c4927635a15c43a5e49b
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-03-17 13:30:05 +0530

    define method swap

--- src/com/ashutosh/corejava/QuickSortDemo2.java
+++ src/com/ashutosh/corejava/QuickSortDemo2.java
@@ -1,6 +1,13 @@
 package com.ashutosh.corejava;
 
 public class QuickSortDemo2 {
+	void swap(int[] arr, int i, int j) {
+		int temp=arr[i];
+		arr[i]=arr[j];
+		arr[j]=temp;
+	}
+	
+	
 
 	public static void main(String[] args) {
 		
