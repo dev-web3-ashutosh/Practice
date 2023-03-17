578a23ed081bdd75949e02115bec8ede1619b55d
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-03-17 13:34:15 +0530

    define method partition

--- src/com/ashutosh/corejava/QuickSortDemo2.java
+++ src/com/ashutosh/corejava/QuickSortDemo2.java
@@ -7,6 +7,19 @@ public class QuickSortDemo2 {
 		arr[j]=temp;
 	}
 	
+	int partition(int[] arr, int l, int h) {
+		int pivot=arr[h];
+		int j=l-1;
+		for(int i=0;i<h;i++) {
+			if(arr[i]>pivot) {
+				j++;
+				swap(arr,i,j);
+			}
+		}
+		swap(arr,(j+1),h);
+		return (j+1);
+	}
+	
 	
 
 	public static void main(String[] args) {
