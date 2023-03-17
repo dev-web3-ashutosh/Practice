efd36d72e4f49cc7964456258be9debd5deb991c
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-03-17 15:15:09 +0530

    define method getMax

--- src/com/ashutosh/corejava/RadixSortDemo.java
+++ src/com/ashutosh/corejava/RadixSortDemo.java
@@ -1,6 +1,18 @@
 package com.ashutosh.corejava;
 
 public class RadixSortDemo {
+	
+	int getMax(int[] arr, int n) {
+		int max=arr[0];
+		for(int i=1;i<n; i++) {
+			if(arr[i]>max) {
+				max=arr[i];
+			}
+		}
+		return max;
+	}
+	
+	
 
 	public static void main(String[] args) {
 		
