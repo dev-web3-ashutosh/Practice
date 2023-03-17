6b882574d637f0f7394171b21c97dd72a9de0282
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-03-17 15:26:31 +0530

    define printArray method

--- src/com/ashutosh/corejava/RadixSortDemo.java
+++ src/com/ashutosh/corejava/RadixSortDemo.java
@@ -45,7 +45,12 @@ public class RadixSortDemo {
 		}
 	}
 	
-	
+	void printArray(int[] arr, int n) {
+		for(int i=0;i<n;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
 
 	public static void main(String[] args) {
 		
