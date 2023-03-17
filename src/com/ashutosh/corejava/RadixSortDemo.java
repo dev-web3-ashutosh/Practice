cfb54ce9e3af102d668f7d30f8d9c08506aec01f
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-03-17 16:18:18 +0530

    adding functionality

--- src/com/ashutosh/corejava/RadixSortDemo.java
+++ src/com/ashutosh/corejava/RadixSortDemo.java
@@ -3,6 +3,7 @@ package com.ashutosh.corejava;
 import java.util.Arrays;
 
 public class RadixSortDemo {
+	static int iter=1;
 	
 	int getMax(int[] arr, int n) {
 		int max=arr[0];
@@ -23,19 +24,28 @@ public class RadixSortDemo {
 		for(i=0;i<n;i++) {
 			count[(arr[i]/e)%10]++;
 		}
+		System.out.println("Iteration - "+iter);
+		System.out.println("Initial count array:-");
+		printArray(count,10);
 		
 		for(i=1;i<10;i++) {
 			count[i]+=count[i-1];
 		}
+		System.out.println("Modified count array:-");
+		printArray(count,10);
 		
 		for(i=n-1;i>=0;i--) {
 			output[count[(arr[i]/e)%10]-1]=arr[i];
 			count[(arr[i]/e)%10]--;
 		}
+		System.out.println("Output array:-");
+		printArray(output,n);
+		System.out.println();
 		
 		for(i=0;i<n;i++) {
 			arr[i]=output[i];
 		}
+		iter++;
 	}
 	
 	void radixSort(int[] arr, int n) {
@@ -54,7 +64,7 @@ public class RadixSortDemo {
 
 	public static void main(String[] args) {
 		RadixSortDemo obj=new RadixSortDemo();
-		int[] arr= {22,34,0,66,8,99,22,0,66,9876};
+		int[] arr= {22,34,0,66,8,99,22,0,66,987};
 		int n=arr.length;
 		System.out.println("Array before sorting:-");
 		obj.printArray(arr,n);
