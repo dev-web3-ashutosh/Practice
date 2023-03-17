647f77175dd7bd27ed68e1e0b05aeb608bf531f1
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-03-17 12:40:45 +0530

    write insertion sort logic

--- src/com/ashutosh/corejava/InsertionSortDemo2.java
+++ src/com/ashutosh/corejava/InsertionSortDemo2.java
@@ -1,9 +1,37 @@
 package com.ashutosh.corejava;
 
 public class InsertionSortDemo2 {
+	void insertionSort2(int[] arr) {
+		int n=arr.length;
+		for(int i=1;i<n;i++) {
+			int key=arr[i];
+			int j=i-1;
+			while((j>=0)&&(arr[j]>key)) {
+				arr[j+1]=arr[j];
+				j--;
+			}
+			arr[j]=key;
+			System.out.println("Array during sorting:-");
+			printArray(arr);
+		}
+	}
+	void printArray(int[] arr) {
+		int n=arr.length;
+		for(int i=0;i<n;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
 
 	public static void main(String[] args) {
-		
+		int[] arr= {22,34,8,99,0};
+		InsertionSortDemo2 obj=new InsertionSortDemo2();
+		System.out.println("Array before sorting:-");
+		obj.printArray(arr);
+		obj.insertionSort2(arr);
+		System.out.println();
+		System.out.println("Sorted array:-");
+		obj.printArray(arr);
 
 	}
 
