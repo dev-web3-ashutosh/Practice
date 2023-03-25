de584935347b572ff12df29cc6c49a5d21dc3fcd
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-03-25 17:25:28 +0530

    define method to print stack elements from bottom to top

--- src/com/ashutosh/corejava/StackDemo.java
+++ src/com/ashutosh/corejava/StackDemo.java
@@ -12,12 +12,26 @@ public class StackDemo {
 		StackDemo.size=size;
 		for(int i=0;i<size-1;i++) {
 			stack.push(rand.nextInt(100));
+			curr_pointer++;
 		}
 		System.out.println("Initial stack");
 		printStack(stack);
 	}
 	
-	
+	void printStack(Stack<Integer> stack) {
+		Stack<Integer> temp=new Stack<Integer>();
+		while(stack.empty()==false) {
+			temp.push(stack.peek());
+			stack.pop();
+		}
+		while(temp.empty()==false) {
+			int t=temp.peek();
+			System.out.print(t+" ");
+			temp.pop();
+			stack.push(t);
+		}
+		System.out.println();
+	}
 
 	public static void main(String[] args) {
 		
