a902265a2dca0a3b48e00be0e12debf0e516e03c
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-03-25 17:37:32 +0530

    delete default constructor

--- src/com/ashutosh/corejava/StackDemo.java
+++ src/com/ashutosh/corejava/StackDemo.java
@@ -7,17 +7,9 @@ import java.util.Stack;
 public class StackDemo {
 	Random rand=new Random();
 	static Stack<Integer> stack=new Stack<Integer>();
-	static int size;
+	static int size=stack.size();
 	static int curr_pointer=-1;
-	StackDemo(int size) {
-		StackDemo.size=size;
-		for(int i=0;i<size;i++) {
-			stack.push(rand.nextInt(100));
-			curr_pointer++;
-		}
-		System.out.println("Initial stack");
-		printStack(stack);
-	}
+	
 	
 	void printStack(Stack<Integer> stack) {
 		Stack<Integer> temp=new Stack<Integer>();
@@ -35,11 +27,8 @@ public class StackDemo {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Enter size of the stack");
-		Scanner sc=new Scanner(System.in);
-		int size=sc.nextInt();
-		StackDemo obj=new StackDemo(size);
-		sc.close();
+		StackDemo obj=new StackDemo();
+		
 	}
 
 }
