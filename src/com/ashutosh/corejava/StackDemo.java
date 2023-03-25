2d148e0f1fead4dc93ca9e2d6a20312c84255bcc
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-03-25 17:10:10 +0530

    create default constructor to create and initialise stack of input size

--- src/com/ashutosh/corejava/StackDemo.java
+++ src/com/ashutosh/corejava/StackDemo.java
@@ -1,6 +1,23 @@
 package com.ashutosh.corejava;
 
+import java.util.Random;
+import java.util.Stack;
+
 public class StackDemo {
+	Random rand=new Random();
+	static Stack<Integer> stack=new Stack<Integer>();
+	static int size;
+	static int curr_pointer=-1;
+	StackDemo(int size) {
+		StackDemo.size=size;
+		for(int i=0;i<size-1;i++) {
+			stack.push(rand.nextInt(100));
+		}
+		System.out.println("Initial stack");
+		printStack(stack);
+	}
+	
+	
 
 	public static void main(String[] args) {
 		
