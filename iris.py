5a2fcdf68507fd832827e683e0ce7bc465745572
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2024-09-13 18:53:54 +0530

    show head

--- iris.py
+++ iris.py
@@ -1,3 +1,4 @@
 import pandas as pd
 names = ["sepal-length", "sepal-width", "petal-length", "petal-width", "species"] 
-data=pd.read_csv("", names=names) 
+data=pd.read_csv("https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv", names=names)
+print(data.head())
