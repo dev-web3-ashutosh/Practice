b08a0cf175cc4b10943846c713ee94661d92c4a4
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2024-09-13 19:23:20 +0530

    Update iris.py

--- iris.py
+++ iris.py
@@ -1,7 +1,11 @@
 import pandas as pd
 names = ["sepal-length", "sepal-width", "petal-length", "petal-width", "species"] 
 data=pd.read_csv("https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv", names=names)
+print("Info")
 data.info()
+print("Head")
 print(data.head())
+print("Shape")
 data.shape
+print("Des")
 data.describe()
