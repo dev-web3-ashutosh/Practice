316c91d9d82cf1363603777fe5c4548994c829d9
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2024-09-13 18:59:53 +0530

    info, shape, describe

--- iris.py
+++ iris.py
@@ -1,4 +1,7 @@
 import pandas as pd
 names = ["sepal-length", "sepal-width", "petal-length", "petal-width", "species"] 
 data=pd.read_csv("https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv", names=names)
+data.info()
 print(data.head())
+print(data.shape())
+data.describe()
