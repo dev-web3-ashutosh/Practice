b90ebc23f96e10d485e12e55b666e8b4d024b11f
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2024-09-14 23:53:38 +0530

    Update iris.py

--- iris.py
+++ iris.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 names = ["sepal-length", "sepal-width", "petal-length", "petal-width", "species"] 
 data=pd.read_csv("https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv", names=names)
 print("Info")
