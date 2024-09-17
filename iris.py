2818a7c36d5f33c48e49f0720b3fb40b47f2a95e
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2024-09-17 20:40:47 +0530

    Update iris.py

--- iris.py
+++ iris.py
@@ -1,7 +1,11 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
+
 names = ["sepal-length", "sepal-width", "petal-length", "petal-width", "species"] 
 data=pd.read_csv("https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv", names=names)
+
+'''
 print("Info")
 data.info()
 print("Head")
@@ -12,3 +16,13 @@ print("Des")
 print(data.describe())
 print("GrpBy")
 print(data.groupby("species").size())
+'''
+
+# Visualisations
+
+# 1. Box and Whisker
+data.plot(type='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+plt.show()
+
+
+
